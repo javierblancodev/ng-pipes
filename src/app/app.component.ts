709608d6b2b84c67e060014a3eba510bf1e67043
Javier Blanco <javierblancoit@gmail.com>
2023-05-01 08:06:35 +0100

    implement custom pipe to convert from miles to kms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   date = '';
   amount = 0;
   height = 0;
+  miles = 0;
 
   onNameChange(value: string) {
     this.name = value;
@@ -25,5 +26,9 @@ export class AppComponent {
 
   onHeightChange(value: string) {
     this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
   }
 }
